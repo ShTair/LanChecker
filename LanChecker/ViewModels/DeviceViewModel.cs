308312a7987f8a4286e483787ea04bf900ce3481
ShTair <git@vsh.jp>
2017-07-23 01:37:37 +0900

    デバイスVMの実装

--- LanChecker/ViewModels/DeviceViewModel.cs
+++ LanChecker/ViewModels/DeviceViewModel.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace LanChecker.ViewModels
 {
-    class DeviceViewModel : INotifyPropertyChanged
+    class DeviceViewModel : INotifyPropertyChanged, IDisposable
     {
         private HashSet<TargetViewModel> _targets;
 
+        #region properties
+
         public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool IsRunning { get; private set; }
 
         public string MacAddress { get; private set; }
 
         public string Name { get; private set; }
+
+        public string Category { get; private set; }
+
+        public DateTime LastReach
+        {
+            get { return _LastReach; }
+            set
+            {
+                if (_LastReach == value) return;
+                _LastReach = value;
+                PropertyChanged?.Invoke(this, _LastReachChangedEventArgs);
+                PropertyChanged?.Invoke(this, _OrderTimeChangedEventArgs);
+            }
+        }
+        private DateTime _LastReach;
+        private PropertyChangedEventArgs _LastReachChangedEventArgs = new PropertyChangedEventArgs(nameof(LastReach));
 
         public TimeSpan Elapsed
         {
@@ -21,6 +42,12 @@ namespace LanChecker.ViewModels
             {
                 if (_Elapsed == value) return;
                 _Elapsed = value;
+
+                if (Elapsed == TimeSpan.Zero) Status = 0;
+                else if (Elapsed < TimeSpan.FromHours(1)) Status = 1;
+                else if (Elapsed < TimeSpan.FromDays(3)) Status = 2;
+                else Status = 3;
+
                 PropertyChanged?.Invoke(this, _ElapsedChangedEventArgs);
             }
         }
@@ -35,21 +62,52 @@ namespace LanChecker.ViewModels
                 if (_Status == value) return;
                 _Status = value;
                 PropertyChanged?.Invoke(this, _StatusChangedEventArgs);
+                PropertyChanged?.Invoke(this, _OrderTimeChangedEventArgs);
             }
         }
         private int _Status;
         private PropertyChangedEventArgs _StatusChangedEventArgs = new PropertyChangedEventArgs(nameof(Status));
 
-        public DeviceViewModel(string mac)
+        public DateTime OrderTime
+        {
+            get { return Status <= 1 ? DateTime.MaxValue : LastReach; }
+        }
+        private PropertyChangedEventArgs _OrderTimeChangedEventArgs = new PropertyChangedEventArgs(nameof(OrderTime));
+
+        #endregion
+
+        public DeviceViewModel(string mac, string name, string category)
         {
             _targets = new HashSet<TargetViewModel>();
 
             MacAddress = mac;
+            Name = name;
+            Category = category;
+
+            IsRunning = true;
+            Task.Run((Action)Run);
         }
 
-        public void Find(TargetViewModel target)
+        private async void Run()
         {
-            _targets.Add(target);
+            while (IsRunning)
+            {
+                await Task.Delay(5000);
+                if (LastReach >= DateTime.Now.AddSeconds(-30)) continue;
+
+                Elapsed = DateTime.Now - LastReach;
+            }
+        }
+
+        public void Reach()
+        {
+            LastReach = DateTime.Now;
+            Elapsed = TimeSpan.Zero;
+        }
+
+        public void Dispose()
+        {
+            IsRunning = false;
         }
     }
 }
